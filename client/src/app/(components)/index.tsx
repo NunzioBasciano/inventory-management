13097e13089d6b0237cb8cd72acf20e1ce9b860c
NunzioBasciano <nbasho1988@gmail.com>
2024-10-04 18:51:31 +0200

    redux config

---
+++ client/src/app/(components)/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Menu } from "lucide-react";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">NunzioStock</h1>
+
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4 " />
+        </button>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex-grow mt-8">{/* LINKS */}</div>
+
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">© 2024 NunzioStock</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
