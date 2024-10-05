c9c71a4fd5fcc0da25bbadc9576160be6e96cec2
NunzioBasciano <nbasho1988@gmail.com>
2024-10-05 16:13:33 +0200

    feat: add server

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           <input
             type="search"
             placeholder="Start type to search groups & products"
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:border-blue-500"
           />
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
             <Bell className="text-gray-500 " size={20} />
