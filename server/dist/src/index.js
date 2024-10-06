6639ef027c7d7eae560d9c238e05b22e2a799588
NunzioBasciano <nbasho1988@gmail.com>
2024-10-06 17:08:07 +0200

    add: dasboardRoutes.ts

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -11,6 +11,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 /* ROUTE IMPORT */
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -22,9 +23,7 @@ app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 /* ROUTES */
-app.get('/hello', (req, res) => {
-    res.send('hello world');
-});
+app.use('/dashboard', dashboardRoutes_1.default); // http://localhost:8000/dashboard
 /* SERVER */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 /* ROUTE IMPORT */
+import dashboardRoutes from './routes/dashboardRoutes'
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,9 +20,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 /* ROUTES */
-app.get('/hello', (req, res) => {
-    res.send('hello world');
-})
+app.use('/dashboard', dashboardRoutes); // http://localhost:8000/dashboard
 
 /* SERVER */
 const port = process.env.PORT || 3001;
