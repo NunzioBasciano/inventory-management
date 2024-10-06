10446747635b7ad2e3c804e51b7c491aed67a7ad
NunzioBasciano <nbasho1988@gmail.com>
2024-10-06 16:06:19 +0200

    add: setup src server

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+/* Questo file configura un'applicazione Express di base, configurando middleware per la gestione delle richieste e della sicurezza, e avviando il server su una porta specificata. */
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+/* ROUTE IMPORT */
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
