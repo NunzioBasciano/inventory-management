a5ce53308772e8450bfb5dcf5130b9df524ebe47
NunzioBasciano <nbasho1988@gmail.com>
2024-10-06 18:36:15 +0200

    add: grid in dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
-
+"use client";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <div className="row-span-3 xl:row-span6 bg-gray-500"></div>
+      <div className="row-span-3 xl:row-span6 bg-gray-500"></div>
+      <div className="row-span-2 xl:row-span3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
+      <div className="row-span-3 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
