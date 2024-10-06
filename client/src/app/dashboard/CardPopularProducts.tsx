85e02c87d5b36daa9de3791ec7a3ea18837be734
NunzioBasciano <nbasho1988@gmail.com>
2024-10-06 19:13:19 +0200

    feat: CardPopularProducts

---
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -0,0 +1,54 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import { ShoppingBag } from "lucide-react";
+import React from "react";
+
+const CardPopularProducts = () => {
+  const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  return (
+    <div className="row-span-3 xl:row-span6 bg-white shadow-md rounded-2xl pb-16 ">
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
+            Popular Products
+          </h3>
+          <hr />
+          <div className="overflow-auto h-full">
+            {dashboardMetrics?.popularProducts.map((product) => (
+              <div
+                key={product.productId}
+                className="flex items-center justify-between gap-3 px-5 py-7 border-b"
+              >
+                <div className="flex items-center gap-3">
+                  <div>img</div>
+                  <div className="flex flex-col justify-between gap-1">
+                    <div className="font-bold text-gray-700">
+                      {product.name}
+                    </div>
+                    <div className="flex text-sm items-center">
+                      <span className="font-bold text-blue-500 text-sm">
+                        {product.price}
+                      </span>
+                      <span className="mx-2">|</span>
+                      {/* <Rating /> */}
+                    </div>
+                  </div>
+                </div>
+                <div className="text-xs flex items-center">
+                  <button className="p-2 rounded-full bg-blue-100 text-blue-600 mr-2">
+                    <ShoppingBag className="w-4 h-4" />
+                  </button>
+                  {Math.round(product.stockQuantity / 1000)}k Sold
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CardPopularProducts;
+1;
--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import CardPopularProducts from "./CardPopularProducts";
+
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
-      <div className="row-span-3 xl:row-span6 bg-gray-500"></div>
+      <CardPopularProducts />
       <div className="row-span-3 xl:row-span6 bg-gray-500"></div>
       <div className="row-span-2 xl:row-span3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
       <div className="row-span-3 bg-gray-500"></div>
