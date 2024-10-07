a4c1cbc6a62560e8186c85b1d31526c2464e790c
NunzioBasciano <nbasho1988@gmail.com>
2024-10-07 15:44:34 +0200

    feat: CardSalesSummary

---
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,9 @@
+const CardPurchaseSummary = () => {
+  return (
+    <div className="row-span-2 xl:row-span3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500">
+      CardPurchaseSummary
+    </div>
+  );
+};
+
+export default CardPurchaseSummary;
--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import CardPopularProducts from "./CardPopularProducts";
+import CardSalesSummary from "./CardSalesSummary";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
-      <div className="row-span-3 xl:row-span6 bg-gray-500"></div>
+      <CardSalesSummary />
       <div className="row-span-2 xl:row-span3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
       <div className="row-span-3 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
