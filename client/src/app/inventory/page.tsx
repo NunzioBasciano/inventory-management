3eaead06292356b6ed40d8230c97d4b031b67bcb
NunzioBasciano <nbasho1988@gmail.com>
2024-10-08 16:21:02 +0200

    feat: DataGrid in inventory page

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -2,6 +2,32 @@
 
 import { useGetProductsQuery } from "@/state/api";
 import Header from "../(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "productId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Product Name", width: 200 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? row.rating : "N/A"),
+  },
+  {
+    field: "stockQuantity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
+];
 
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
@@ -19,6 +45,13 @@ const Inventory = () => {
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+        className="bg-white rounded-lg border border-gray-200 mt-5 !text-gray-700 "
+      />
     </div>
   );
 };
