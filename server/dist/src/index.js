0147d514e9b33f6559b3a918c7e1a38b0ab747f5
NunzioBasciano <nbasho1988@gmail.com>
2024-10-08 15:39:24 +0200

    feat: BE for products page

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -12,6 +12,7 @@ const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 /* ROUTE IMPORT */
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -24,6 +25,7 @@ app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 /* ROUTES */
 app.use('/dashboard', dashboardRoutes_1.default); // http://localhost:8000/dashboard
+app.use('/products', productRoutes_1.default); // http://localhost:8000/products
 /* SERVER */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 /* ROUTE IMPORT */
 import dashboardRoutes from './routes/dashboardRoutes'
+import productRoutes from './routes/productRoutes'
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use('/dashboard', dashboardRoutes); // http://localhost:8000/dashboard
+app.use('/products', productRoutes); // http://localhost:8000/products
 
 /* SERVER */
 const port = process.env.PORT || 3001;
