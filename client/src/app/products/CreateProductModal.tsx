68e0b4005b8568d6896038dbf19c0c9375c8819c
NunzioBasciano <nbasho1988@gmail.com>
2024-10-08 18:07:16 +0200

    fix: css in createProductModal, feat: userController in BE

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -114,7 +114,7 @@ const CreateProductModal = ({
           {/* CREATE ACTION */}
           <button
             type="submit"
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
           >
             Create
           </button>
